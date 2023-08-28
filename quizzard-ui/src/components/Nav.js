6946c616b6e857ab9d7d009326f5cff4758c1afb
EleneTs <elene.tsiramua@ah.nl>
2023-08-28 23:58:40 +0400

    Navigation Bar for pages

--- quizzard-ui/src/components/Nav.js
+++ quizzard-ui/src/components/Nav.js
@@ -2,7 +2,37 @@ import React from 'react';
 
 function Nav(props) {
     return (
-        <div></div>
+        <div className="master-nav">
+            <div className="logo">
+                <img src="/media/baba-logo.svg" alt="Logo"/>
+                <span>quizzard</span>
+            </div>
+            <div className="icons">
+                <a className="link" href="#">
+                    <i className="fa-solid fa-gamepad"></i>
+                    <span>Play</span>
+                </a>
+
+                <a className="link" href="#">
+                    <i className="fa-regular fa-circle-question"></i>
+                    <span>Create Quiz</span>
+                </a>
+
+                <a className="link" href="#">
+                    <i className="fa-solid fa-medal"></i>
+                    <span>Leaderboard</span>
+                </a>
+
+                <a className="link" href="#">
+                    <i className="fa-solid fa-user-group"></i>
+                    <span>Friends</span>
+                </a>
+
+                <a className="auth-dp" href="">
+                    <img src="/media/default-dp.png" alt="dp"/>
+                </a>
+            </div>
+        </div>
     );
 }
 
